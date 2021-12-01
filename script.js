169176c803ef3e8c7a1117cf56bdb7258ef34c49
GunelYusuf <91114321+GunelYusuf@users.noreply.github.com>
2021-12-01 20:24:56 +0400

    added ShowAllStudents functions

--- script.js
+++ script.js
@@ -1,35 +1,40 @@
-let cars=[
-      {"marka":"Jaguar", "model":"XJ","color":"black","year":2018},
-      {"marka":"Porsche", "model":"Cayenne","color":"white","year":2021},
-      {"marka":"Mazda", "model":"CX-7","color":"silver","year":2020},
-      {"marka":"Chevrolet", "model":"Camaro","color":"white","year":2017},
-      {"marka":"Volkswagen", "model":"Caravelle","color":"black","year":2021}
-    ];
+// let cars=[
+//       {"marka":"Jaguar", "model":"XJ","color":"black","year":2018},
+//       {"marka":"Porsche", "model":"Cayenne","color":"white","year":2021},
+//       {"marka":"Mazda", "model":"CX-7","color":"silver","year":2020},
+//       {"marka":"Chevrolet", "model":"Camaro","color":"white","year":2017},
+//       {"marka":"Volkswagen", "model":"Caravelle","color":"black","year":2021}
+//     ];
 
-  for (let i = 0; i < cars.length; i++) {
-   console.log(`${cars[2].marka} ${cars[2].color}`);
-   console.log(cars[i]);
-   //    console.log(cars[i].model);
-   //    console.log(cars[i].color);
-   //    console.log(cars[i].year);
-   }
+//   for (let i = 0; i < cars.length; i++) {
+//    console.log(`${cars[2].marka} ${cars[2].color}`);
+//    console.log(cars[i]);
+//    //    console.log(cars[i].model);
+//    //    console.log(cars[i].color);
+//    //    console.log(cars[i].year);
+//    }
+ 
 
 
  class Group {
-    constructor(id,name,students = []) {
+    constructor(id,name,studentCount) {
         this.id = id;
         this.name = name;
-        const studentCount=6;
-        this.students = students;
+        this.studentCount=studentCount;
+       
     }
-   
+    
+    students = [];
+    
     addStudent(studentToAdd){  
-        if(this.students.length==6)
+        if(this.students.length==this.studentCount)
         {
             console.log(`${studentToAdd.name} don't add because The number of people in the ${this.name} group is maximum`);
+            console.log(this.students.length);
+            return;
         }    
         else if (this.students.some(s => studentToAdd.id === s.id)) {
-            console.log(` ${studentToAdd.name} has already been added to the group!`)
+            console.log(` ${studentToAdd.id} has already been added to the group!`)
         } 
         else
         {  
@@ -38,7 +43,10 @@ let cars=[
         } 
         return this.students;
     }
-
+    showAllStudents(){
+        this.students.forEach(a=>console.log(`Id: ${a.id} Name: ${a.name} Surname: ${a.lastname} Age: ${a.age}`));
+        
+    }
 }
 class Student {
     constructor(id,name,lastname, age) {
@@ -49,7 +57,7 @@ class Student {
     }
 }
 
-const p125 = new Group(1,"P125");
+const p125 = new Group(1,"P125",6);
 const newStu1=new Student(1,"Gunel","Yusubova",26);
 const newStu2=new Student(2,"Sebine","Quliyeva",26);
 const newStu3=new Student(3,"Elchin","Yusubov",23);
@@ -57,14 +65,20 @@ const newStu4=new Student(4,"Raul","Rustemli",28);
 const newStu5=new Student(5,"Ayshen","Abdullayeva",25);
 const newStu6=new Student(6,"Lavinya","Yusuf",25);
 const newStu7=new Student(7,"Simran","Qocayev",28);
+// const newStu8=new Student(8,"Khanish","Alesgerov",16);
 
 p125.addStudent(newStu1);
+
 p125.addStudent(newStu2);
+
+// p125.addStudent(newStu1);
 p125.addStudent(newStu3);
 p125.addStudent(newStu4);
 p125.addStudent(newStu5);
 p125.addStudent(newStu6);
 p125.addStudent(newStu7);
+// p125.addStudent(newStu8);
+p125.showAllStudents();
 
 
 
